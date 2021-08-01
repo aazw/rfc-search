d165a7e2e641a15b2e33dd5e792c9348683db3bb
aazw <3594744+aazw@users.noreply.github.com>
2021-08-01 04:25:22 +0000

    fix script for github actions

--- scripts/download_rfc.sh
+++ scripts/download_rfc.sh
@@ -6,4 +6,4 @@ ls -a $SCRIPT_DIR/../rfc/
 
 curl -iL https://www.rfc-editor.org/in-notes/tar/RFC-all.zip -o $SCRIPT_DIR/../rfc/RFC-all.zip
 cd $SCRIPT_DIR/../rfc/ 
-unzip RFC-all.zip
+unzip -v -o RFC-all.zip -x "*.pdf" || true
