6caafdc75ab1e19ec917cba05d251a9b641e03ee
aazw <3594744+aazw@users.noreply.github.com>
2021-08-01 14:41:24 +0900

    fix script for github actions

--- scripts/download_rfc.sh
+++ scripts/download_rfc.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
+set -eu
+
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-
-ls -a $SCRIPT_DIR/../rfc/
-
-curl -iL https://www.rfc-editor.org/in-notes/tar/RFC-all.zip -o $SCRIPT_DIR/../rfc/RFC-all.zip
 cd $SCRIPT_DIR/../rfc/ 
-unzip -v -o RFC-all.zip -x "*.pdf" || true
+
+curl -iL https://www.rfc-editor.org/in-notes/tar/RFC-all.zip -o RFC-all.zip
+unzip -o RFC-all.zip -x "*.pdf" || true
