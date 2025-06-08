e74caeb102bd5d15ba4ecb877e0baff95357748d
aazw <3594744+aazw@users.noreply.github.com>
2025-06-08 23:56:42 +0900

    update Dev Container

--- scripts/download_rfc.sh
+++ scripts/download_rfc.sh
@@ -2,8 +2,8 @@
 
 set -eu
 
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
-cd $SCRIPT_DIR/../rfc/ 
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
+cd $SCRIPT_DIR/../rfc/
 
 curl -iL https://www.rfc-editor.org/in-notes/tar/RFC-all.zip -o RFC-all.zip
 unzip -o RFC-all.zip -x "*.pdf" || true
--- scripts/remove_ds_store.sh
+++ scripts/remove_ds_store.sh
@@ -4,7 +4,7 @@ set -eu
 
 # How do I get the directory where a Bash script is located from within the script itself?
 # https://stackoverflow.com/questions/59895/how-do-i-get-the-directory-where-a-bash-script-is-located-from-within-the-script
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
 cd ${SCRIPT_DIR}/..
 
-find . -name ".DS_Store" | xargs rm
+find . -name ".DS_Store" -delete
